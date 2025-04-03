0f99fb63dc779b4f96fcfec7332edabe3e513846
Vilem Zavodny <vilem.zavodny@espressif.com>
2025-04-03 13:38:14 +0200

    no message

--- examples/display/pytest_display.py
+++ examples/display/pytest_display.py
@@ -2,8 +2,33 @@
 # SPDX-License-Identifier: CC0-1.0
 
 import pytest
+import cv2
 from pytest_embedded import Dut
-from ..pytest_helpers import bsp_test_image
+
+
+def bsp_capture_image(image_path):
+    # Return video from the first webcam on your computer.
+    cap = cv2.VideoCapture(0)
+    # reads frames from a camera
+    # ret checks return at each frame
+    ret, frame = cap.read()
+    if ret:
+        # TODO: Change size image
+        # TODO: Crop image
+
+        # Save image
+        cv2.imwrite(image_path, frame)
+        print(f"Image saved {image_path}")
+    else:
+        print("Cannot save image.")
+
+    # Close the window / Release webcam
+    cap.release()
+
+
+def bsp_test_image(board, example, expectation):
+    image_file = f"snapshot_{example}_{board}.jpg"
+    bsp_capture_image(image_file)
 
 
 @pytest.mark.esp_box_3
